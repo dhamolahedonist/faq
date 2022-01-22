2d19248dd48a9661f0931dcebd86bcb78c0a7436
Samuel Oduba <samueloduba123@gmail.com>
2022-01-22 09:10:37 -0800

    updated faq

--- faq.js
+++ faq.js
@@ -1,15 +1,19 @@
-function showDiv1() {
-    document.getElementById('div1').style.display = 'block';
-    document.getElementById('div2').style.display = 'none';
-    document.getElementById('div3').style.display = 'none';
+function hideAll() {
+    const toggleElements = document.querySelectorAll('.faq') // This is an array because there are more than 1 element.
+    for(i = 0; i < toggleElements.length; i++) {
+        // Get the element we are dealing with and set its display to none
+        toggleElements[i].style.display = 'none'
+    }
 }
-function showDiv2() {
-    document.getElementById('div1').style.display = 'none';
-    document.getElementById('div2').style.display = 'block';
-    document.getElementById('div3').style.display = 'none';
+
+hideAll()
+
+function toggle(divId) {
+    const display = document.getElementById(divId).style.display
+    hideAll() 
+    if (display == 'none') {
+        document.getElementById(divId).style.display = 'block'
+    }
+   
 }
-function showDiv3() {
-    document.getElementById('div1').style.display = 'none';
-    document.getElementById('div2').style.display = 'none';
-    document.getElementById('div3').style.display = 'block';
-}
+
